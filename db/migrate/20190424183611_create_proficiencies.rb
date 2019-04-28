46c161e5cf27729a75efeeccdfa9da9ac3599988
github username <github email address>
2019-04-28 21:10:27 +0100

    added a stat modifier signal for skills

--- db/migrate/20190424183611_create_proficiencies.rb
+++ db/migrate/20190424183611_create_proficiencies.rb
@@ -2,6 +2,7 @@ class CreateProficiencies < ActiveRecord::Migration[5.2]
   def change
     create_table :proficiencies do |t|
       t.string :name
+      t.string :mod
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_183708) do
 
   create_table "proficiencies", force: :cascade do |t|
     t.string "name"
+    t.string "mod"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
